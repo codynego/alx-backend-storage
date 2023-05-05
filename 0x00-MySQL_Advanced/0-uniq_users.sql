ba9c6890f34f37a00843fdfb2c56e2f488d3c4a7
codynego <emonenaabednego@gmail.com>
2023-05-05 15:46:15 +0100

    sql script to create a table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create a user table with the following attribute
+-- id, email and name
+
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
