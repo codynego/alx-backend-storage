397b42bf17f32b2d71b6610419e2ab6d12793c49
codynego <emonenaabednego@gmail.com>
2023-05-06 18:19:28 +0100

    some mysql files

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+--  a SQL script that lists all bands with Glam rock as their main style
+-- ranked by their longevity
+
+SELECT band_name, (YEAR(split) - YEAR(formed)) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
