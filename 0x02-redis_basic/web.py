d083066cfefe35d5f4421ad78e6139ca4e599f6b
codynego <emonenaabednego@gmail.com>
2023-05-10 19:36:32 +0100

    web count and caching

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Implementing an expiring web cache and tracker
+"""
+from functools import wraps
+import redis
+from typing import List, Union
+import requests
+cache = redis.Redis()
+
+def page_count(method):
+    """
+    get page count
+    """
+
+    @wraps(method)
+    def wrapper(url):
+        """
+        the wrapper
+        """
+        cache_key = "cached:" + url
+        cache_data = cache.get(cache_key)
+        htmlpage = method(url)
+        if cache_data == None:
+            cache.set(cache_key, htmlpage)
+
+        count_key = "count:" + url
+        cache.incr(count_key)
+        cache.expire(cache_key, 10)
+        return htmlpage
+    return wrapper
+
+
+@page_count
+def get_page(url: str) -> str:
+    page = requests.get(url)
+    return page.text
