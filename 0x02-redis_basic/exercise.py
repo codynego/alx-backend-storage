3706d49c850f30ba6c893859fe92310bef61a95a
codynego <emonenaabednego@gmail.com>
2023-05-10 14:01:58 +0100

    a

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 from uuid import uuid4
-from typing import List, Union
+from typing import List, Union, Callable, Optional
 
 
 class Cache:
@@ -29,3 +29,17 @@ class Cache:
         key: str = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, int, bytes, float]:
+        """
+        A method gets a value from redis
+        Args:
+            key (str): the key
+            fn (Optional[Callable]): a callable
+        Return:
+            returns the value
+        """
+        value: str = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
